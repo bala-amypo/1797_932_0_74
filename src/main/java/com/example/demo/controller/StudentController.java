1a4cce66aa54ad813121ee64450993047e34b4d3
bala-amypo <bala@amypo.in>
2025-12-17 09:30:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,11 +1,6 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,26 +19,11 @@ public class StudentController {
     public StudentEntity addStudents(@RequestBody StudentEntity student){
         return ser.addStudents(student);
     }
-    
+
     @GetMapping("/getStudents")
-    public List<StudentEntity> getStudents() {
+    public List<StudentEntity> getStudents() 
+    {
         return ser.getStudents();
     }
     
-    @GetMapping("/getStudent/{id}")
-    public StudentEntity getStudentById(@PathVariable Long id) {
-        return ser.getStudentById(id);
-    }
-
-    @DeleteMapping("/deleteStudent/{id}")
-    public String deleteStudentById(@PathVariable Long id) {
-        StudentEntity student = ser.getStudentById(id);
-        if(student.isPresent()) {
-            ser.deleteStudentById(id);
-            return "Student deleted successfully.";
-        } else {
-            return "Student not found.";
-        }
-    }
-
 }
